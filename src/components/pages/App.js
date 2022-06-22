c985ca254ba85601825418efb40c7a6edfec8b5a
Kris <magikabdul@users.noreply.github.com>
2022-06-22 17:12:29 +0200

    [WOR] storybook (#1)
    * [WOR] - added storybook
    
    * [WOR] - added absolute imports
    
    * [WOR] - project structure cleaning

---
+++ src/components/pages/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Button from 'components/atoms/Button';
+
+const App = () => (
+  <div>
+    <Button/>
+  </div>
+)
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import ReactDOM from 'react-dom/client';
+import App from 'components/pages/App';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );

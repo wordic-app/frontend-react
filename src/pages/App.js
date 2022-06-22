09968e7ecc14ad17ea045d1a236877ac55b6edc5
Kris <magikabdul@users.noreply.github.com>
2022-06-23 01:39:37 +0200

    [WOR] styled components (#3)
    * [WOR] - prop-types moved to dependencies
    
    * [WOR] - GlobalStyles and ThemeProvider

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from 'components/atoms/Button';
+import GlobalStyles from 'theme/GlobalStyles';
+import lightTheme from 'theme/lightTheme';
+import darkTheme from 'theme/darkTheme';
+import { ThemeProvider } from 'styled-components';
 
-const App = () => (
-  <div>
-    <Button />
-  </div>
-);
+const App = () => {
+  const [darkMode, setDarkMode] = useState(true);
+  return (
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <GlobalStyles theme={darkMode ? darkTheme : lightTheme} />
+      <div>
+        <Button onClick={() => setDarkMode(!darkMode)} />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam
+          consectetur dignissimos ex fuga maiores nesciunt soluta totam
+          voluptates. Accusantium architecto ducimus earum facere facilis iure
+          maxime molestiae, officiis voluptas voluptates.
+        </p>
+      </div>
+    </ThemeProvider>
+  );
+};
 
 export default App;

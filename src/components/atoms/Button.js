906ff3d1a737ad9d40bc244fe64731a615268658
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2022-06-23 01:37:03 +0200

    [WOR] - GlobalStyles and ThemeProvider

--- src/components/atoms/Button.js
+++ src/components/atoms/Button.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Button = () => <button type='button'>button</button>;
+const Button = ({ onClick }) => (
+  <button type='button' onClick={onClick}>
+    button
+  </button>
+);
+
+Button.propTypes = {
+  onClick: PropTypes.func,
+};
+
+Button.defaultProps = {
+  onClick: undefined,
+};
 
 export default Button;

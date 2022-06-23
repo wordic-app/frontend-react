5bd26785f8aaeef3f9bc5c20119a533a23b2c550
Kris <magikabdul@users.noreply.github.com>
2022-06-24 01:14:19 +0200

    [WOR-1] button component (#6)
    * [WOR-1] - theming configuration for storybook
    
    * [WOR-1] - added Button.js component

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,10 @@
+import { addDecorator } from '@storybook/react';
+import { withThemesProvider } from "storybook-addon-styled-component-theme";
+import { ThemeProvider } from 'styled-components';
+import lightTheme from '../src/theme/lightTheme';
+import darkTheme from '../src/theme/darkTheme';
+import GlobalStyles from '../src/theme/GlobalStyles';
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,3 +14,8 @@ export const parameters = {
     },
   },
 }
+
+const themes = [lightTheme, darkTheme];
+
+addDecorator(withThemesProvider(themes, ThemeProvider));
+addDecorator(s => <><GlobalStyles theme={darkTheme}/>{s()}</>);
--- src/components/atoms/Button.stories.js
+++ src/components/atoms/Button.stories.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import Button from 'components/atoms/Button';
+import { FaFacebook } from 'react-icons/fa';
 
 export default {
-  title: 'Button',
+  title: 'Atoms/Button',
   component: Button,
 };
 
 const Template = (args) => <Button {...args} />;
 
-export const Primary = Template.bind({});
+export const TextButton = Template.bind({});
+TextButton.args = {
+  color: 'primary',
+  variant: 'default',
+  size: 'medium',
+  children: 'Text Button',
+};
+
+export const TextAndIconButton = Template.bind({});
+TextAndIconButton.args = {
+  color: 'primary',
+  variant: 'default',
+  size: 'medium',
+  children: 'FACEBOOK',
+  Icon: FaFacebook,
+};
+
+export const IconButton = Template.bind({});
+IconButton.args = {
+  color: 'primary',
+  variant: 'default',
+  size: 'medium',
+  Icon: FaFacebook,
+};

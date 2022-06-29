f0637b5210a3d21a79a93589ac8df511f2359cda
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2022-06-30 00:30:05 +0200

    [WOR-47] - added empty MainPage.js

---
+++ src/pages/MainPage/MainPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+import Header from 'pages/MainPage/Header/Header';
+
+const Container = styled.div`
+  width: 100%;
+`;
+
+const MainPage = () => (
+  <Container>
+    <Header />
+  </Container>
+);
+
+export default MainPage;
